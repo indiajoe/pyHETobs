3669b4185ef3597dbbc31db572f4270de7974b1a
Joe Philip Ninan <indiajoe@gmail.com>
2018-02-25 02:37:01 -0500

    Print the start and end time

--- pyHETobs/ObservationPlanning.py
+++ pyHETobs/ObservationPlanning.py
@@ -53,8 +53,10 @@ if __name__ == "__main__":
         else:
             print('Target :{0} not observable in East or West track'.format(star.name))
 
+    DicOfStartAndEndTimes = {}
     # Now plot the apertures at night
     for starname in ObservationBlockSets:
+        DicOfStartAndEndTimes[starname] = []
         for ObsBlock in ObservationBlockSets[starname]:
             if ObsBlock is None:
                 continue
@@ -62,11 +64,17 @@ if __name__ == "__main__":
             Tarray = np.linspace(ObsBlock.max_start_time,ObsBlock.max_end_time,100)
             aper = ObsBlock.EffectiveHETapperture(Tarray)
             TimeList = [ObsBlock.Transit_time + TimeDelta(t*u.second) for t in Tarray]
+            DicOfStartAndEndTimes[starname].append((TimeList[0],TimeList[-1]))
             plt.plot_date([t.plot_date for t in TimeList],aper,'.',color='k',alpha=0.5)
             plt.text(ObsBlock.Transit_time.plot_date, max(aper),
                      starname+'({0})'.format(int(round(ObsBlock.Optimal_Azimuth.value))))
 
     
+    # Print the table of observable windows
+    print('='*10)
+    for star in DicOfStartAndEndTimes:
+        print('{0} : {1}'.format(star,DicOfStartAndEndTimes[star]))
+    print('='*10)
     plt.axvline(McD.sun_set_time(NightOfObs,which='previous').plot_date,color='r')
     plt.axvline(McD.sun_rise_time(NightOfObs,which='next').plot_date,linestyle='dashed', color='r')
     plt.axvline(McD.moon_set_time(NightOfObs,which='next').plot_date,color='yellow')
